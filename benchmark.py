477062c9fd1d65a9d5fae15b2f1a218ae8d5765f
ben-w-conrad <159072715+ben-w-conrad@users.noreply.github.com>
2024-12-10 15:02:35 -0800

    Add Multilingual Support (#16)

--- benchmark.py
+++ benchmark.py
@@ -10,21 +10,25 @@ import editdistance
 
 from dataset import *
 from engine import *
-from normalizer import Normalizer
+from languages import Languages
+from normalizer import EnglishNormalizer
 
-WorkerResult = namedtuple('WorkerResult', ['num_errors', 'num_words', 'audio_sec', 'process_sec'])
+WorkerResult = namedtuple(
+    "WorkerResult", ["num_errors", "num_words", "audio_sec", "process_sec"]
+)
 RESULTS_FOLDER = os.path.join(os.path.dirname(__file__), "results")
 
 
 def process(
-        engine: Engines,
-        engine_params: Dict[str, Any],
-        dataset: Datasets,
-        dataset_folder: str,
-        indices: Sequence[int]) -> WorkerResult:
-    engine = Engine.create(engine, **engine_params)
-    dataset = Dataset.create(dataset, folder=dataset_folder)
-    normalizer = Normalizer()
+    engine: Engines,
+    engine_params: Dict[str, Any],
+    language: Languages,
+    dataset: Datasets,
+    dataset_folder: str,
+    indices: Sequence[int],
+) -> WorkerResult:
+    engine = Engine.create(engine, language=language, **engine_params)
+    dataset = Dataset.create(dataset, folder=dataset_folder, language=language)
 
     error_count = 0
     word_count = 0
@@ -33,10 +37,19 @@ def process(
 
         transcript = engine.transcribe(audio_path)
 
-        ref_sentence = ref_transcript.strip('\n ').lower()
-        ref_words = normalizer.to_american(normalizer.normalize_abbreviations(ref_sentence)).split()
-        transcribed_sentence = transcript.strip('\n ').lower()
-        transcribed_words = normalizer.to_american(normalizer.normalize_abbreviations(transcribed_sentence)).split()
+        ref_sentence = ref_transcript.strip("\n ").lower()
+        transcribed_sentence = transcript.strip("\n ").lower()
+
+        if language == Languages.EN:
+            ref_words = EnglishNormalizer.to_american(
+                EnglishNormalizer.normalize_abbreviations(ref_sentence)
+            ).split()
+            transcribed_words = EnglishNormalizer.to_american(
+                EnglishNormalizer.normalize_abbreviations(transcribed_sentence)
+            ).split()
+        else:
+            ref_words = ref_sentence.split()
+            transcribed_words = transcribed_sentence.split()
 
         error_count += editdistance.eval(ref_words, transcribed_words)
         word_count += len(ref_words)
@@ -47,68 +60,99 @@ def process(
         num_errors=error_count,
         num_words=word_count,
         audio_sec=engine.audio_sec(),
-        process_sec=engine.process_sec())
+        process_sec=engine.process_sec(),
+    )
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--engine', required=True, choices=[x.value for x in Engines])
-    parser.add_argument('--dataset', required=True, choices=[x.value for x in Datasets])
-    parser.add_argument('--dataset-folder', required=True)
-    parser.add_argument('--aws-profile')
-    parser.add_argument('--azure-speech-key')
-    parser.add_argument('--azure-speech-location')
-    parser.add_argument('--google-application-credentials')
-    parser.add_argument('--deepspeech-pbmm')
-    parser.add_argument('--deepspeech-scorer')
-    parser.add_argument('--picovoice-access-key')
-    parser.add_argument('--watson-speech-to-text-api-key')
-    parser.add_argument('--watson-speech-to-text-url')
-    parser.add_argument('--num-examples', type=int, default=None)
-    parser.add_argument('--num-workers', type=int, default=os.cpu_count())
+    parser.add_argument("--engine", required=True, choices=[x.value for x in Engines])
+    parser.add_argument("--dataset", required=True, choices=[x.value for x in Datasets])
+    parser.add_argument("--dataset-folder", required=True)
+    parser.add_argument(
+        "--language", required=True, choices=[x.value for x in Languages]
+    )
+    parser.add_argument("--aws-profile")
+    parser.add_argument("--azure-speech-key")
+    parser.add_argument("--azure-speech-location")
+    parser.add_argument("--google-application-credentials")
+    parser.add_argument("--deepspeech-pbmm")
+    parser.add_argument("--deepspeech-scorer")
+    parser.add_argument("--picovoice-access-key")
+    parser.add_argument("--picovoice-model-path", default=None)
+    parser.add_argument("--picovoice-library-path", default=None)
+    parser.add_argument("--watson-speech-to-text-api-key")
+    parser.add_argument("--watson-speech-to-text-url")
+    parser.add_argument("--num-examples", type=int, default=None)
+    parser.add_argument("--num-workers", type=int, default=os.cpu_count())
     args = parser.parse_args()
 
     engine = Engines(args.engine)
     dataset_type = Datasets(args.dataset)
+    language = Languages(args.language)
     dataset_folder = args.dataset_folder
     num_examples = args.num_examples
     num_workers = args.num_workers
 
     engine_params = dict()
-    if engine is Engines.AMAZON_TRANSCRIBE:
+    if engine == Engines.AMAZON_TRANSCRIBE:
         if args.aws_profile is None:
             raise ValueError("`aws-profile` is required")
-        os.environ['AWS_PROFILE'] = args.aws_profile
-    elif engine is Engines.AZURE_SPEECH_TO_TEXT:
+        os.environ["AWS_PROFILE"] = args.aws_profile
+    elif engine == Engines.AZURE_SPEECH_TO_TEXT:
         if args.azure_speech_key is None or args.azure_speech_location is None:
-            raise ValueError("`azure-speech-key` and `azure-speech-location` are required")
-        engine_params['azure_speech_key'] = args.azure_speech_key
-        engine_params['azure_speech_location'] = args.azure_speech_location
-    elif engine is Engines.GOOGLE_SPEECH_TO_TEXT or engine == Engines.GOOGLE_SPEECH_TO_TEXT_ENHANCED:
+            raise ValueError(
+                "`azure-speech-key` and `azure-speech-location` are required"
+            )
+        engine_params["azure_speech_key"] = args.azure_speech_key
+        engine_params["azure_speech_location"] = args.azure_speech_location
+    elif (
+        engine == Engines.GOOGLE_SPEECH_TO_TEXT
+        or engine == Engines.GOOGLE_SPEECH_TO_TEXT_ENHANCED
+    ):
         if args.google_application_credentials is None:
             raise ValueError("`google-application-credentials` is required")
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = args.google_application_credentials
-    elif engine is Engines.PICOVOICE_CHEETAH:
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = (
+            args.google_application_credentials
+        )
+    elif engine == Engines.PICOVOICE_CHEETAH:
         if args.picovoice_access_key is None:
             raise ValueError("`picovoice-access-key` is required")
-        engine_params['access_key'] = args.picovoice_access_key
-    elif engine is Engines.PICOVOICE_LEOPARD:
+        if args.picovoice_model_path is None and args.language != Languages.EN:
+            raise ValueError("`picovoice-model-path` is required for non-EN languages")
+        engine_params["access_key"] = args.picovoice_access_key
+        engine_params["model_path"] = args.picovoice_model_path
+        engine_params["library_path"] = args.picovoice_library_path
+    elif engine == Engines.PICOVOICE_LEOPARD:
         if args.picovoice_access_key is None:
             raise ValueError("`picovoice-access-key` is required")
-        engine_params['access_key'] = args.picovoice_access_key
-    elif engine is Engines.IBM_WATSON_SPEECH_TO_TEXT:
-        if args.watson_speech_to_text_api_key is None or args.watson_speech_to_text_url is None:
-            raise ValueError("`watson-speech-to-text-api-key` and `watson-speech-to-text-url` are required")
-        engine_params['watson_speech_to_text_api_key'] = args.watson_speech_to_text_api_key
-        engine_params['watson_speech_to_text_url'] = args.watson_speech_to_text_url
+        if args.picovoice_model_path is None and args.language != Languages.EN:
+            raise ValueError("`picovoice-model-path` is required for non-EN languages")
+        engine_params["access_key"] = args.picovoice_access_key
+        engine_params["model_path"] = args.picovoice_model_path
+        engine_params["library_path"] = args.picovoice_library_path
+    elif engine == Engines.IBM_WATSON_SPEECH_TO_TEXT:
+        if (
+            args.watson_speech_to_text_api_key is None
+            or args.watson_speech_to_text_url is None
+        ):
+            raise ValueError(
+                "`watson-speech-to-text-api-key` and `watson-speech-to-text-url` are required"
+            )
+        engine_params["watson_speech_to_text_api_key"] = (
+            args.watson_speech_to_text_api_key
+        )
+        engine_params["watson_speech_to_text_url"] = args.watson_speech_to_text_url
 
-    dataset = Dataset.create(dataset_type, folder=dataset_folder)
+    dataset = Dataset.create(dataset_type, folder=dataset_folder, language=language)
     indices = list(range(dataset.size()))
     random.shuffle(indices)
     if args.num_examples is not None:
         indices = indices[:num_examples]
 
     chunk = math.ceil(len(indices) / num_workers)
+
+    print(f"Processing {len(indices)} examples...")
 
     futures = []
     with ProcessPoolExecutor(num_workers) as executor:
@@ -117,9 +161,10 @@ def main():
                 process,
                 engine=engine,
                 engine_params=engine_params,
+                language=language,
                 dataset=dataset_type,
                 dataset_folder=dataset_folder,
-                indices=indices[i * chunk: (i + 1) * chunk]
+                indices=indices[i * chunk : (i + 1) * chunk],
             )
             futures.append(future)
 
@@ -131,15 +176,17 @@ def main():
     rtf = sum(x.process_sec for x in res) / sum(x.audio_sec for x in res)
     word_error_rate = 100 * float(num_errors) / num_words
 
-    results_log_path = os.path.join(RESULTS_FOLDER, dataset_type.value, f"{str(engine)}.log")
+    results_log_path = os.path.join(
+        RESULTS_FOLDER, language.value, dataset_type.value, f"{str(engine)}.log"
+    )
     os.makedirs(os.path.dirname(results_log_path), exist_ok=True)
     with open(results_log_path, "w") as f:
         f.write(f"WER: {str(word_error_rate)}\n")
         f.write(f"RTF: {str(rtf)}\n")
 
-    print(f'WER: {word_error_rate:.2f}')
-    print(f'RTF: {rtf}')
+    print(f"WED: {word_error_rate:.2f}")
+    print(f"RTF: {rtf}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- plot_results.py
+++ plot_results.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from typing import Tuple
+from typing import Dict, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,85 +8,114 @@ import numpy as np
 from benchmark import RESULTS_FOLDER
 from dataset import Datasets
 from engine import Engines
-from results import WER, RTF
+from results import *
 
 Color = Tuple[float, float, float]
+
 
 def rgb_from_hex(x: str) -> Color:
     x = x.strip("# ")
     assert len(x) == 6
     return int(x[:2], 16) / 255, int(x[2:4], 16) / 255, int(x[4:], 16) / 255
 
+
 BLACK = rgb_from_hex("#000000")
-GREY1 = rgb_from_hex("#3F3F3F")
+GREY1 = rgb_from_hex("#4F4F4F")
 GREY2 = rgb_from_hex("#5F5F5F")
-GREY3 = rgb_from_hex("#7F7F7F")
-GREY4 = rgb_from_hex("#9F9F9F")
-GREY5 = rgb_from_hex("#BFBFBF")
+GREY3 = rgb_from_hex("#6F6F6F")
+GREY4 = rgb_from_hex("#7F7F7F")
+GREY5 = rgb_from_hex("#8F8F8F")
 WHITE = rgb_from_hex("#FFFFFF")
 BLUE = rgb_from_hex("#377DFF")
 
 ENGINE_PRINT_NAMES = {
-    Engines.AMAZON_TRANSCRIBE: 'Amazon',
-    Engines.AZURE_SPEECH_TO_TEXT: 'Azure',
-    Engines.GOOGLE_SPEECH_TO_TEXT: 'Google',
-    Engines.GOOGLE_SPEECH_TO_TEXT_ENHANCED: 'Google\nEnhanced',
-    Engines.IBM_WATSON_SPEECH_TO_TEXT: 'IBM',
-    Engines.WHISPER_TINY: 'Whisper\nTiny',
-    Engines.WHISPER_BASE: 'Whisper\nBase',
-    Engines.WHISPER_SMALL: 'Whisper\nSmall',
-    Engines.WHISPER_MEDIUM: 'Whisper\nMedium',
-    Engines.WHISPER_LARGE: 'Whisper\nLarge\n(Multilingual)',
-    Engines.PICOVOICE_CHEETAH: 'Picovoice\nCheetah',
-    Engines.PICOVOICE_LEOPARD: 'Picovoice\nLeopard'
+    Engines.AMAZON_TRANSCRIBE: "Amazon",
+    Engines.AZURE_SPEECH_TO_TEXT: "Azure",
+    Engines.GOOGLE_SPEECH_TO_TEXT: "Google",
+    Engines.GOOGLE_SPEECH_TO_TEXT_ENHANCED: "Google\nEnhanced",
+    Engines.IBM_WATSON_SPEECH_TO_TEXT: "IBM",
+    Engines.WHISPER_TINY: "Whisper\nTiny",
+    Engines.WHISPER_BASE: "Whisper\nBase",
+    Engines.WHISPER_SMALL: "Whisper\nSmall",
+    Engines.WHISPER_MEDIUM: "Whisper\nMedium",
+    Engines.WHISPER_LARGE: "Whisper\nLarge",
+    Engines.PICOVOICE_CHEETAH: "Picovoice\nCheetah",
+    Engines.PICOVOICE_LEOPARD: "Picovoice\nLeopard",
 }
 
 ENGINE_COLORS = {
     Engines.AMAZON_TRANSCRIBE: GREY5,
     Engines.AZURE_SPEECH_TO_TEXT: GREY4,
-    Engines.GOOGLE_SPEECH_TO_TEXT: GREY2,
-    Engines.GOOGLE_SPEECH_TO_TEXT_ENHANCED: GREY2,
-    Engines.IBM_WATSON_SPEECH_TO_TEXT: GREY1,
-    Engines.WHISPER_TINY: GREY3,
-    Engines.WHISPER_BASE: GREY3,
-    Engines.WHISPER_SMALL: GREY3,
-    Engines.WHISPER_MEDIUM: GREY3,
-    Engines.WHISPER_LARGE: GREY3,
-    Engines.PICOVOICE_CHEETAH: BLUE,
+    Engines.GOOGLE_SPEECH_TO_TEXT: GREY3,
+    Engines.GOOGLE_SPEECH_TO_TEXT_ENHANCED: GREY3,
+    Engines.IBM_WATSON_SPEECH_TO_TEXT: GREY2,
+    Engines.WHISPER_LARGE: GREY1,
+    Engines.WHISPER_MEDIUM: GREY1,
+    Engines.WHISPER_SMALL: GREY1,
+    Engines.WHISPER_BASE: GREY1,
+    Engines.WHISPER_TINY: GREY1,
     Engines.PICOVOICE_LEOPARD: BLUE,
+    Engines.PICOVOICE_CHEETAH: BLUE,
 }
 
-def _plot_wer(save_folder: str, show: bool = False) -> None:
-    sorted_wer = sorted(
-        [(e, round(sum(w for w in WER[e].values()), 1) / len(Datasets)) for e in Engines], key=lambda x: x[1])
-    print('\n'.join(f"{e.value}: {x:.2f}" for e, x in sorted_wer))
 
-    fig, ax = plt.subplots(figsize=(12, 6))
+def _plot_wer(
+    engine_wer: Dict[Engines, Dict[Datasets, float]], save_path: str, show: bool = False
+) -> None:
+    sorted_wer = sorted(
+        [
+            (
+                e,
+                round(
+                    sum(w for w in engine_wer[e].values()) / len(engine_wer[e]) + 1e-9,
+                    1,
+                ),
+            )
+            for e in engine_wer.keys()
+        ],
+        key=lambda x: x[1],
+    )
+    print("\n".join(f"{e.value}: {x}" for e, x in sorted_wer))
+
+    _, ax = plt.subplots(figsize=(12, 6))
 
     for i, (engine, wer) in enumerate(sorted_wer, start=1):
         color = ENGINE_COLORS[engine]
         ax.bar([i], [wer], 0.4, color=color)
-        ax.text(i - 0.3, wer + 1, f'{wer:.1f}%', color=color)
+        ax.text(
+            i,
+            wer + 0.5,
+            f"{wer}%",
+            color=color,
+            ha="center",
+            va="bottom",
+        )
 
     for spine in plt.gca().spines.values():
-        if spine.spine_type != 'bottom' and spine.spine_type != 'left':
+        if spine.spine_type != "bottom" and spine.spine_type != "left":
             spine.set_visible(False)
 
-    plt.xticks(np.arange(1, len(Engines) + 1), [ENGINE_PRINT_NAMES[x[0]] for x in sorted_wer], fontsize=9)
-    plt.yticks(np.arange(5, 30, 5), ["%s%%" % str(x) for x in np.arange(5, 30, 5)])
-    plt.ylabel('Word Error Rate (lower is better)')
+    plt.xticks(
+        np.arange(1, len(engine_wer) + 1),
+        [ENGINE_PRINT_NAMES[x[0]] for x in sorted_wer],
+        fontsize=9,
+    )
+    ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: f"{x:.0f}%"))
+    plt.ylabel("Word Error Rate (lower is better)")
 
-    plot_path = os.path.join(save_folder, "WER.png")
-    os.makedirs(os.path.dirname(plot_path), exist_ok=True)
-    plt.savefig(plot_path)
-    print(f"Saved plot to `{plot_path}`")
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    plt.savefig(save_path)
+    print(f"Saved plot to `{save_path}`")
 
     if show:
         plt.show()
 
     plt.close()
 
-def _plot_cpu(save_folder: str, show: bool, dataset: Datasets = Datasets.TED_LIUM) -> None:
+
+def _plot_cpu(
+    save_folder: str, show: bool, dataset: Datasets = Datasets.TED_LIUM
+) -> None:
     fig, ax = plt.subplots(figsize=(6, 6))
     x_limit = 0
     for engine_type, engine_value in RTF.items():
@@ -99,7 +128,8 @@ def _plot_cpu(save_folder: str, show: bool, dataset: Datasets = Datasets.TED_LIU
             height=0.5,
             color=ENGINE_COLORS[engine_type],
             edgecolor="none",
-            label=ENGINE_PRINT_NAMES[engine_type])
+            label=ENGINE_PRINT_NAMES[engine_type],
+        )
         ax.text(
             core_hour + 30,
             ENGINE_PRINT_NAMES[engine_type],
@@ -107,7 +137,8 @@ def _plot_cpu(save_folder: str, show: bool, dataset: Datasets = Datasets.TED_LIU
             ha="center",
             va="center",
             fontsize=12,
-            color=ENGINE_COLORS[engine_type])
+            color=ENGINE_COLORS[engine_type],
+        )
 
     ax.spines["top"].set_visible(False)
     ax.spines["bottom"].set_visible(False)
@@ -115,7 +146,10 @@ def _plot_cpu(save_folder: str, show: bool, dataset: Datasets = Datasets.TED_LIU
     plt.xlim([0, x_limit + 50])
     ax.set_xticks([])
     ax.set_ylim([-0.5, 5.5])
-    plt.title("Core-hour required to process 100 hours of audio (lower is better)", fontsize=12)
+    plt.title(
+        "Core-hour required to process 100 hours of audio (lower is better)",
+        fontsize=12,
+    )
     plot_path = os.path.join(save_folder, "cpu_usage_comparison.png")
     os.makedirs(os.path.dirname(plot_path), exist_ok=True)
     plt.savefig(plot_path)
@@ -134,7 +168,12 @@ def main() -> None:
 
     save_folder = os.path.join(RESULTS_FOLDER, "plots")
 
-    _plot_wer(save_folder=save_folder, show=args.show)
+    _plot_wer(WER, save_path=os.path.join(save_folder, "WER.png"), show=args.show)
+    _plot_wer(WER_FR, save_path=os.path.join(save_folder, "WER_FR.png"), show=args.show)
+    _plot_wer(WER_DE, save_path=os.path.join(save_folder, "WER_DE.png"), show=args.show)
+    _plot_wer(WER_ES, save_path=os.path.join(save_folder, "WER_ES.png"), show=args.show)
+    _plot_wer(WER_IT, save_path=os.path.join(save_folder, "WER_IT.png"), show=args.show)
+    _plot_wer(WER_PT, save_path=os.path.join(save_folder, "WER_PT.png"), show=args.show)
     _plot_cpu(save_folder=save_folder, show=args.show, dataset=Datasets.TED_LIUM)
 
 
